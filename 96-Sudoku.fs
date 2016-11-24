548cbb21583d9d9a45a025b0a0a53396260ffb14
MendelMonteiro <mendel.monteiro@gmail.com>
2016-11-24 22:09:13 +0100

    Get vertical rows

--- 96-Sudoku.fs
+++ 96-Sudoku.fs
@@ -19,20 +19,24 @@ module Sudoku =
         xs |> List.iter (sb.Append >> ignore)
         sb.ToString()
     
+    [<StructuredFormatDisplay("({col}, {row}) => {value}")>]
     type Cell = 
         { row : int
           col : int
           value : int }
     
+    [<StructuredFormatDisplay("{cells}")>]
     type Nonet = 
         { cells : Cell list
           rows : Cell list list
           cols : Cell list list }
     
+    [<StructuredFormatDisplay("{cell}")>]
     type CellInNonet = 
         { cell : Cell
           nonet : Nonet }
     
+    [<StructuredFormatDisplay("{nonet}")>]
     type NonetInBoard = 
         { nonet : Nonet
           left : Nonet option
@@ -42,6 +46,7 @@ module Sudoku =
     
     type Line = CellInNonet list
     
+    [<StructuredFormatDisplay("{name} {nonets}")>]
     type Board = 
         { name : string
           nonets : Nonet list
@@ -59,52 +64,84 @@ module Sudoku =
         | x :: xs -> [ xss |> List.map List.head ]
         | _ -> []
     
-    let parseFile rawLines = 
-        let parseNonetFromRaw horizontalPos nonetLines = 
-            let parseNonetLine r chars = 
-                chars |> List.mapi (fun c x -> 
-                             { row = r
-                               col = c
-                               value = System.Int32.Parse(x.ToString()) })
-            
-            // 3 lines with other nonets in it
-            let vals = 
-                nonetLines
-                |> List.take NonetSize
-                |> List.mapi (fun i x -> 
-                       (explode x
-                        |> List.skip (horizontalPos * NonetSize)
-                        |> List.take NonetSize
-                        |> parseNonetLine i))
-                |> List.concat
-            
-            let r = vals |> List.chunkBySize NonetSize
-            { cells = vals
-              rows = r
-              cols = r |> transpose }
+    let printLines xs = 
+        printfn "["
+        xs |> List.iter (fun x -> 
+                  printf "\t[ "
+                  List.iter (printf "%A; ") x
+                  printfn "]")
+        printfn "]"
+    
+    let rec printManyLines xss =
+        printfn "----- %A" (List.length xss)
+        match xss with
+        | xs :: xss -> printLines xs 
+                       printManyLines xss
+        | [] -> ()
+
+    let parseNonetFromRaw nonetLines horizontalPos = 
+        let parseNonetLine r chars = 
+            chars |> List.mapi (fun c x -> 
+                         { row = r
+                           col = c
+                           value = System.Int32.Parse(x.ToString()) })
         
-        let parseBoard (name :: nonetLines : string list) = 
+        // 3 lines with other nonets in it
+        let vals = 
+            nonetLines
+            |> List.take NonetSize
+            |> List.mapi (fun i x -> 
+                   (explode x
+                    |> List.skip (horizontalPos * NonetSize)
+                    |> List.take NonetSize
+                    |> parseNonetLine i))
+            |> List.concat
+        
+        let r = vals |> List.chunkBySize NonetSize
+        { cells = vals; rows = r; cols = r |> transpose }
+    
+    let attachCellsToNonets f xs = 
+        xs |> List.map (fun x -> 
+                  x
+                  |> f
+                  |> List.map (fun y -> 
+                         y |> List.map (fun z -> 
+                                  { cell = z
+                                    nonet = x })))
+    
+    let parseFile rawLines = 
+        let parseBoard (name :: rawNonetLines : string list) = 
             let getHorizontalLines xs = 
-                let foo = 
-                    xs |> List.map (fun x -> 
-                              x.rows |> List.map (fun y -> 
-                                            y |> List.map (fun z -> 
-                                                     { cell = z
-                                                       nonet = x })))
-                foo
+                xs
+                |> attachCellsToNonets (fun x -> x.rows)
+                |> List.chunkBySize (BoardSize)
+                |> List.collect (transpose)
+                |> List.map (fun xs -> xs |> List.concat)
+            
+            let getVerticalLines xs = 
+                xs
+                |> attachCellsToNonets (fun x -> x.cols)
+                |> List.concat
+                |> List.chunkBySize (NonetSize)
+                |> List.chunkBySize (BoardSize)
                 |> transpose
-                |> (List.map List.concat)
+                |> List.collect transpose
+                |> List.map List.concat
             
-            let nonets = [ 0..2 ] |> List.map (fun x -> nonetLines |> parseNonetFromRaw x)
+            //printfn "Board name %A" name
+            //printfn "Raw lines %A" rawNonetLines
+            let nonets = 
+                rawNonetLines
+                |> List.chunkBySize NonetSize
+                |> List.collect (fun chunk -> [ 0..(NonetSize - 1) ] |> List.map (parseNonetFromRaw chunk))
             
-            //printfn "%A" nonets
+            printfn "Nonets %A" nonets.Length
             let board = 
                 { name = name
                   nonets = nonets
                   horizontalLines = (nonets |> getHorizontalLines)
-                  verticalLines = [] }
+                  verticalLines = (nonets |> getVerticalLines) }
             board
-        
         rawLines
         |> List.chunkBySize (1 + NonetSize * BoardSize)
         |> List.take 1
@@ -115,4 +152,6 @@ module Sudoku =
         let rawLines = System.IO.File.ReadLines(filePath)
         let board = parseFile (rawLines |> List.ofSeq) |> List.head
         printfn "Horizontal lines"
-        board.horizontalLines |> List.iter (printfn "%A")
+        board.horizontalLines |> printLines
+        printfn "Vertical lines"
+        board.verticalLines |> printLines
